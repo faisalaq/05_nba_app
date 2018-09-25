87e4cb224d269fc3d1219c4e925024c081315d38
Faisal <faq01818@gmail.com>
2018-09-25 12:38:40 +0530

    S05-L56, added LoadMore function to NewsList card functionality

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import axios from 'axios'
 import { URL } from '../../../config'
+import styles from './newsList.css'
 
 class NewsList extends Component {
 
@@ -14,6 +15,16 @@ class NewsList extends Component {
     }
 
     componentWillMount(){
+        this.request(this.state.start, this.state.end)
+    }
+
+    loadMore(){
+        let end = this.state.end + this.state.amount;
+
+        this.request(this.state.end, end)
+    }
+
+    request(start, end){
         axios.get(`${URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
         .then(response => {
             this.setState({
@@ -22,12 +33,39 @@ class NewsList extends Component {
         })
     }
 
+    renderNews = (type) => {
+        let template = null; 
+
+        switch(type) {
+            case('card'):
+            console.log(type)
+                template = this.state.items.map((item, i)=>(
+                    <div key={i}>
+                        <div className={styles.newsList_item} >
+                            <Link to={`/articles/${item.id}`}>
+                                <h2>{item.title}</h2>
+                            </Link>
+
+                        </div>
+                    </div>
+                ))
+                break;
+            default:
+                template=null;  
+        }
+        return template;
+    }
+
     render() {
         console.log(this.state.items)
         return (
             <div>
-                Hello
+                {this.renderNews(this.props.type)}
+                <div onClick = {()=>this.loadMore()}>
+                    Load More
+                </div>
             </div>
+            
         )
     }
 }
